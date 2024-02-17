17e2ab8b95eeb27ea8549ac210086f4de89cdab8
Varun Santhanam <talkto@vsanthanam.com>
2024-02-17 12:43:20 -0800

    Create #URLString macro in FoundationExtensions

---
+++ Sources/FoundationExtensionsCompilerPlugin/URLStringMacro.swift
@@ -0,0 +1,62 @@
+// SwiftExtensions
+// URLStringMacro.swift
+//
+// MIT License
+//
+// Copyright (c) 2023 Varun Santhanam
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the  Software), to deal
+//
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED  AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+import MacroExtensions
+import SwiftCompilerPlugin
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import SwiftSyntaxMacros
+
+public struct URLStringMacro: ExpressionMacro {
+
+    // MARK: - ExpressionMacro
+
+    public static func expansion(
+        of node: some FreestandingMacroExpansionSyntax,
+        in context: some MacroExpansionContext
+    ) throws -> ExprSyntax {
+        guard case let .stringSegment(literal) = try node.argumentList.first?.expression
+            .as(StringLiteralExprSyntax.self)?
+            .segments.first
+            .unwrap("#URLString requires a single string literal expression") else {
+            throw MacroError("#URLString requires a single string literal expression")
+        }
+
+        return try "\"\(raw: evaluate(literal.content.text))\""
+    }
+
+    // MARK: - Private
+
+    private static func evaluate(_ string: String) throws -> String {
+        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+        guard let match = detector.firstMatch(in: string, options: [], range: NSRange(location: 0, length: string.utf16.count)),
+              match.range.length == string.utf16.count else {
+            throw MacroError("\"\(string)\" is not a valid URL")
+        }
+        return string
+    }
+
+}
